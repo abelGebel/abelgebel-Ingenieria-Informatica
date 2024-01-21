8f3b7daf7aa180e81f9dbcf0e982acddfc2348ae
abelGebel <131565277+abelGebel@users.noreply.github.com>
2024-01-21 16:33:29 -0300

    Update Main.java
    Modifico archivo main.java de ProyectoGit desde GitHub

--- Programación Concurrente/ProyectoGit/src/ejmeploGit/Main.java
+++ Programación Concurrente/ProyectoGit/src/ejmeploGit/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Modifico el archivo Main.java");
+		System.out.println("Agrego algo desde GitHub");
 	}
 
 }
